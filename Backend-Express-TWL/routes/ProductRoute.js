346c8cf3091ec1211243078d48a8af97f94b35ff
SamDePaull <prima2100016041@webmail.uad.ac.id>
2023-07-13 11:12:10 +0700

    authmiddle semua produvt

--- Backend-Express-TWL/routes/ProductRoute.js
+++ Backend-Express-TWL/routes/ProductRoute.js
@@ -20,16 +20,16 @@ const router = express.Router();
 router.get('/products',authMiddleware, getProducts);
 
 // Mendapatkan produk berdasarkan ID
-router.get('/products/:id', getProductById);
+router.get('/products/:id',authMiddleware, getProductById);
 
 // Menyimpan produk baru
-router.post('/products', saveProduct);
+router.post('/products',authMiddleware, saveProduct);
 
 // Memperbarui produk berdasarkan ID
-router.patch('/products/:id', updateProduct);
+router.patch('/products/:id',authMiddleware, updateProduct);
 
 // Menghapus produk berdasarkan ID
-router.delete('/products/:id', deleteProduct);
+router.delete('/products/:id',authMiddleware, deleteProduct);
 
 // Registrasi pengguna baru
 router.post('/register', register);
